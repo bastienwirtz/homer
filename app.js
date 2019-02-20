4877ec98e668b1741d7a2a4d3e4a177ec495db39
Bastien Wirtz <bastien.wirtz@gmail.com>
2019-02-19 22:20:46 -0800

    Vertical layout support

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = new Vue({
         config: null,
         offline: false,
         filter: '',
+        vlayout: true
     },
     created: function () {
         let that = this;
@@ -44,6 +45,34 @@ const app = new Vue({
             });
         },
     }
+});
+
+Vue.component('service', {
+    props: ['item'],
+    template: `<div>
+    <div class="card">
+        <a :href="item.url">
+            <div class="card-content">
+                <div class="media">
+                    <div v-if="item.logo" class="media-left">
+                        <figure class="image is-48x48">
+                            <img :src="item.logo" />
+                        </figure>
+                    </div>
+                    <div v-if="item.icon" class="media-left">
+                        <figure class="image is-48x48">
+                            <i style="font-size: 35px" :class="item.icon"></i>
+                        </figure>
+                    </div>
+                    <div class="media-content">
+                        <p class="title is-4">{{ item.name }}</p>
+                        <p class="subtitle is-6">{{ item.subtitle }}</p>
+                    </div>
+                </div>
+                <strong class="tag" v-if="item.tag">#{{ item.tag }}</strong>
+            </div>
+        </a>
+    </div></div>`
 });
 
 if ('serviceWorker' in navigator) {
