1017cc9864f9d34fd5573901534881167402b103
Tiger Oakes <contact@tigeroakes.com>
2020-10-23 13:56:51 -0700

    Update vue config

--- vue.config.js
+++ vue.config.js
@@ -10,50 +10,11 @@ module.exports = {
   publicPath: "",
   pwa: {
     manifestPath: "assets/manifest.json",
-    manifestOptions: {
-      start_url: "../",
-    },
     appleMobileWebAppStatusBarStyle: "black",
     appleMobileWebAppCapable: "yes",
     name: "Homer Dashboard",
-    short_name: "Homer",
-    theme_color: "#3367D6",
-    icons: [
-      {
-        src: "./icons/favicon-16x16.png",
-        sizes: "16x16",
-        type: "image/png",
-      },
-      {
-        src: "./icons/favicon-32x32.png",
-        sizes: "32x32",
-        type: "image/png",
-      },
-      {
-        src: "./icons/icon-any.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "./icons/icon-any.svg",
-        sizes: "any",
-        type: "image/svg+xml",
-        purpose: "any",
-      },
-      {
-        src: "./icons/icon-maskable.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
-      },
-      {
-        src: "./icons/safari-pinned-tab.svg",
-        sizes: "any",
-        type: "image/svg+xml",
-        purpose: "monochrome",
-      },
-    ],
+    themeColor: "#3367D6",
+    manifestOptions: require('./public/assets/manifest.json'),
     iconPaths: {
       favicon32: "assets/icons/favicon-32x32.png",
       favicon16: "assets/icons/favicon-16x16.png",
