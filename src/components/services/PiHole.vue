4ce53b68ea7143433bac39aa955a49981ceb6536
Bram Ceulemans <bramceulemans@me.com>
2020-12-10 00:07:53 +0100

    Fixed wrongly removed computed property

--- src/components/services/PiHole.vue
+++ src/components/services/PiHole.vue
@@ -20,13 +20,13 @@
                 <template v-if="item.subtitle">
                   {{ item.subtitle }}
                 </template>
-                <template v-else-if="status">
+                <template v-else-if="api">
                   {{ percentage }}&percnt; blocked
                 </template>
               </p>
             </div>
-            <div v-if="status" class="status" :class="status.status">
-              {{ status.status }}
+            <div v-if="api" class="status" :class="api.status">
+              {{ api.status }}
             </div>
           </div>
           <div class="tag" :class="item.tagstyle" v-if="item.tag">
@@ -45,15 +45,15 @@ export default {
     item: Object,
   },
   data: () => ({
-    status: {
+    api: {
       status: "",
       ads_percentage_today: 0,
     },
   }),
   computed: {
     percentage: function () {
-      if (this.status) {
-        return this.status.ads_percentage_today.toFixed(1);
+      if (this.api) {
+        return this.api.ads_percentage_today.toFixed(1);
       }
       return "";
     },
@@ -64,7 +64,7 @@ export default {
   methods: {
     fetchStatus: async function () {
       const url = `${this.item.url}/api.php`;
-      this.status = await fetch(url)
+      this.api = await fetch(url)
         .then((response) => response.json())
         .catch((e) => console.log(e));
     },
