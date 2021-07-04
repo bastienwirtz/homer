018fdcd833268d2b8ab54b230a63b4294a4cc246
luixal <luixal@gmail.com>
2021-07-04 11:53:43 +0200

    Refactors groups layout to solve horizontal layout problem in #152

---
+++ src/components/ServiceGroupTitle.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2 v-if="group.name" class="group-title">
+    <i v-if="group.icon" :class="['fa-fw', group.icon]"></i>
+    <div v-else-if="group.logo" class="group-logo media-left">
+      <figure class="image is-48x48">
+        <img :src="group.logo" :alt="`${group.name} logo`" />
+      </figure>
+    </div>
+    {{ group.name }}
+  </h2>
+</template>
+
+<script>
+export default {
+  name: "ServiceGroupTitle",
+  props: {
+    group: { type: Object, required: true },
+  },
+};
+</script>
