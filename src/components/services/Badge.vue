7de73543b87bc29b866467a011bf30d805a8a07a
Frédéric Pégé <frederic.pege@gmail.com>
2022-01-15 23:36:41 +0100

    Create Badge.vue
    Signed-off-by: Frédéric Pégé <frederic.pege@gmail.com>

---
+++ src/components/services/Badge.vue
@@ -0,0 +1,26 @@
+<template>
+    <Generic :item="item">
+    <template #indicator>
+        <div>
+        <img :src=item.badge>
+        </div>
+    </template>
+    </Generic>
+</template>
+
+<script>
+import service from "@/mixins/service.js";
+import Generic from "./Generic.vue";
+
+export default {
+    name: "Badge",
+    mixins: [service],
+    props: {
+        item: Object,
+    },
+    components: {
+        Generic,
+    }
+    
+};
+</script>
