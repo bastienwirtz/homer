03c1a8841d04cb6c46fc8f06e6c384fbdd5c754f
toa <github@uli-fahrer.de>
2022-06-26 17:33:26 +0200

    fix: improve Pi-hole text positioning
    Before the Pi-hole indicator was placed right after the
    service name. This causes longer service names getting
    truncated.
    
    This commit places the uptime indactor in the upper right
    corner.

--- src/components/services/PiHole.vue
+++ src/components/services/PiHole.vue
@@ -60,8 +60,11 @@ export default {
 
 <style scoped lang="scss">
 .status {
+  position: absolute;
   font-size: 0.8rem;
   color: var(--text-title);
+  top: 0.3em;
+  right: 0.5em;
 
   &.enabled:before {
     background-color: #94e185;
