2f1aff4dc5115232b16faf20e85ac4a90677661a
Bastien Wirtz <bastien.wirtz@gmail.com>
2022-07-04 22:17:41 +0200

    Fix icons path

--- vite.config.js
+++ vite.config.js
@@ -22,21 +22,15 @@ export default defineConfig({
         theme_color: "#3367D6",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "./icons/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "./icons/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
         ],
       },
     }),
