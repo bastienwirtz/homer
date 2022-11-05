beb5755747316be8b1d9579d0e194d5a35714f3d
Bastien Wirtz <bastien.wirtz@gmail.com>
2022-11-05 22:14:32 +0100

    Rework speedtesttracker based on the Generic component

--- src/components/services/SpeedtestTracker.vue
+++ src/components/services/SpeedtestTracker.vue
@@ -1,61 +1,43 @@
 <template>
-  <div>
-    <div class="card" :class="item.class">
-      <a :href="item.url" :target="item.target" rel="noreferrer">
-        <div class="card-content">
-          <div class="media">
-            <div v-if="item.logo" class="media-left">
-              <figure class="image is-48x48">
-                <img :src="item.logo" :alt="`${item.name} logo`" />
-              </figure>
-            </div>
-            <div v-if="item.icon" class="media-left">
-              <figure class="image is-48x48">
-                <i style="font-size: 35px" :class="['fa-fw', item.icon]"></i>
-              </figure>
-            </div>
-            <div class="media-content">
-              <p class="title is-4">{{ item.name }}</p>
-              <p class="subtitle is-6">
-                <template v-if="item.subtitle">
-                  {{ item.subtitle }}
-                </template>
-                <template v-else-if="api">
-                  <i class="fas fa-arrow-down"></i> {{ download }} Mbit/s | <i class="fas fa-arrow-up"></i> {{ upload }} Mbit/s | <i class="fas fa-stopwatch"></i> {{ ping }} ms
-                </template>
-              </p>
-            </div>
-          </div>
-          <div class="tag" :class="item.tagstyle" v-if="item.tag">
-            <strong class="tag-text">#{{ item.tag }}</strong>
-          </div>
-        </div>
-      </a>
-    </div>
-  </div>
+  <Generic :item="item">
+    <template #content>
+      <p class="title is-4">{{ item.name }}</p>
+      <p class="subtitle is-6">
+        <template v-if="speedtest">
+          <i class="fas fa-arrow-down"></i> {{ download }} Mbit/s |
+          <i class="fas fa-arrow-up"></i> {{ upload }} Mbit/s |
+          <i class="fas fa-stopwatch"></i> {{ ping }} ms
+        </template>
+      </p>
+    </template>
+  </Generic>
 </template>
 
 <script>
+import service from "@/mixins/service.js";
+import Generic from "./Generic.vue";
+
 export default {
   name: "SpeedtestTracker",
+  mixins: [service],
   props: {
     item: Object,
   },
+  components: {
+    Generic,
+  },
   data: () => ({
-    api: null,
+    speedtest: null,
   }),
   computed: {
-    download: function() {
-      var download = this.api.data.download;
-      return parseFloat(download).toFixed(2);
+    download: function () {
+      return this.format(this.speedtest?.download);
     },
-    upload: function() {
-      var upload = this.api.data.upload;
-      return parseFloat(upload).toFixed(2);
+    upload: function () {
+      return this.format(this.speedtest?.upload);
     },
-    ping: function() {
-      var ping = this.api.data.ping;
-      return parseFloat(ping).toFixed(2);
+    ping: function () {
+      return this.format(this.speedtest?.ping);
     },
   },
   created() {
@@ -63,24 +45,15 @@ export default {
   },
   methods: {
     fetchStatus: async function () {
-      if (this.item.subtitle != null) return; // omitting unnecessary ajax call as the subtitle is showing
-      const url = `${this.item.url}/api/speedtest/latest`;
-      this.api = await fetch(url)
-        .then(function(response) {
-          if (!response.ok) {
-            throw new Error("Not 2xx response");
-          } else {
-            return response.json();
-          }
+      this.fetch("/api/speedtest/latest")
+        .then((response) => {
+          this.speedtest = response.data;
         })
         .catch((e) => console.log(e));
+    },
+    format: function (value) {
+      return value ? parseFloat(value).toFixed(2) : "n/a";
     },
   },
 };
 </script>
-
-<style scoped lang="scss">
-.media-left img {
-  max-height: 100%;
-}
-</style>
