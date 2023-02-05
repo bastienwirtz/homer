c41da691b971f312445b8df701683811105be39d
Rumplin <35449956+rumplin@users.noreply.github.com>
2023-02-05 10:55:27 +0100

    code optimization for service.js
    There are a few optimizations in the code that could lead to a slight improvement:
    ⋅⋅* Computed property **endpoint** is used to avoid repeating the same code.
    ⋅⋅* Object destructuring and spread operator are used to merge the options.
    ⋅⋅* Ternary operator is used to determine the **credentials** value in **options**.
    ⋅⋅* Template literals are used to build the URL, instead of concatenating strings with the **+** operator.

--- src/mixins/service.js
+++ src/mixins/service.js
@@ -2,48 +2,28 @@ export default {
   props: {
     proxy: Object,
   },
-  created: function () {
+  computed: {
     // custom service often consume info from an API using the item link (url) as a base url,
     // but sometimes the base url is different. An optional alternative URL can be provided with the "endpoint" key.
-    this.endpoint = this.item.endpoint || this.item.url;
-
-    if (this.endpoint.endsWith("/")) {
-      this.endpoint = this.endpoint.slice(0, -1);
+    endpoint() {
+      return this.item.endpoint || this.item.url.replace(/\/$/, '');
     }
   },
   methods: {
-    fetch: function (path, init, json = true) {
-      let options = {};
-
-      if (this.proxy?.useCredentials) {
-        options.credentials = "include";
-      }
-
-      // Each item can override the credential settings
-      if (this.item.useCredentials !== undefined) {
-        options.credentials =
-          this.item.useCredentials === true ? "include" : "omit";
-      }
-
-      options = Object.assign(options, init);
-
-      if (path.startsWith("/")) {
-        path = path.slice(1);
-      }
-
-      let url = this.endpoint;
-
-      if (path) {
-        url = `${this.endpoint}/${path}`;
-      }
-
-      return fetch(url, options).then((response) => {
-        if (!response.ok) {
-          throw new Error("Not 2xx response");
-        }
-
-        return json ? response.json() : response;
-      });
+    fetch(path, init, json = true) {
+      let options = {
+		    // Each item can override the credential settings
+        credentials: this.item.useCredentials !== undefined ?
+          this.item.useCredentials ? 'include' : 'omit' :
+          this.proxy?.useCredentials ? 'include' : 'omit'
+      };
+      options = { ...options, ...init };
+      let url = this.endpoint + '/' + (path[0] === '/' ? path.slice(1) : path);
+      return fetch(url, options)
+        .then(response => {
+          if (!response.ok) throw new Error("Not 2xx response");
+          return json ? response.json() : response;
+        });
     },
   },
 };
