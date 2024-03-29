0dd98ed0c3d1131b16ca85eaa4660e1cac4c373d
Vijayakumar Ravi <im@vijayakumar.xyz>
2024-03-29 19:47:13 +0530

    🎉 feat(config): on Fri Mar 29 19:47:13 IST 2024
    Signed-off-by: Vijayakumar Ravi <im@vijayakumar.xyz>

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { createApp, h } from "vue";
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+ 
 import App from "./App.vue";
 
 import "@fortawesome/fontawesome-free/css/all.css";
 import "./assets/app.scss";
  
 const app = createApp(App);
+
+injectSpeedInsights();
 inject();
 
 app.component("DynamicStyle", (_props, context) => {
