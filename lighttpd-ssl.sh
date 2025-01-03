d9e550d59e4936741bbd7f78c33364be5079aa93
羽先生 <8655163+VergilGao@users.noreply.github.com>
2025-01-03 10:03:52 +0800

    format lighttpd-ssl.sh

--- lighttpd-ssl.sh
+++ lighttpd-ssl.sh
@@ -2,5 +2,6 @@
 
 # Enable SSL if needed
 if [[ "${SSL_ENABLE}" == "1" ]]; then
-        echo 'ssl.engine = "enable"'                                                                                                                                                                                                                                echo 'ssl.pemfile = "/www/certificate.pem"'
+    echo 'ssl.engine = "enable"'
+    echo 'ssl.pemfile = "/www/certificate.pem"'
 fi
