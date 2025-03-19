50acb9957e81f75bb28747ad1434132c3c5cb752
Cees Bos <dev@ceesbos.nl>
2025-03-19 22:25:34 +0100

    Add Linkding support

---
+++ src/components/services/Linkding.vue
@@ -0,0 +1,68 @@
+<template>
+  <template v-for="bookmark in bookmarks">
+      <Generic :item="bookmark">
+    </Generic>
+  </template>
+</template>
+
+<script>
+import service from "@/mixins/service.js";
+import Generic from "./Generic.vue";
+
+export default {
+  name: "Linkding",
+  components: {
+    Generic,
+  },
+  mixins: [service],
+  props: {
+    item: Object
+  },
+  data: () => ({
+    bookmarks: [],
+  }),
+  computed: {
+    calculatedLimit: function () {
+      let limit = this.item.hasOwnProperty('limit') ? this.item['limit'] : 5;
+      return Math.min(Math.max(limit,1),15);
+    }
+  },
+  created() {
+    this.fetchBookmarks();
+  },
+  methods: {
+    fetchBookmarks: async function () {
+      const headers = {
+        Authorization: `Token ${this.item.token}`,
+        Accept: "application/json",
+      };
+
+      let limit = this.calculatedLimit;
+
+      let query = ''
+      if (this.item.query) {
+        query = `&q=${encodeURIComponent(this.item.query)}`;
+      }
+
+      let url = `/api/bookmarks/?limit=${limit}${query}`;
+
+      this.fetch(url, {
+        headers,
+      })
+          .then((ld_response) => {
+            this.bookmarks = ld_response.results.map(bookmark => ({
+              name: `${bookmark.title}`,
+              subtitle: `${bookmark.description}`,
+              url: bookmark.url,
+              logo: `${bookmark.favicon_url}`,
+              tag: `${bookmark.tag_names.join(" #")}`
+            }));
+          })
+          .catch((e) => {
+            console.log(e);
+            this.error = true;
+          });
+    },
+  },
+};
+</script>
